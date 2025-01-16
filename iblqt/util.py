5d2a534b19a66baa5d0550bd522d1ea897b9217a
Florian Rau <bimac@users.noreply.github.com>
2025-01-16 17:27:57 +0000

    various widgets for Alyx

--- iblqt/util.py
+++ iblqt/util.py
@@ -2,16 +2,23 @@
 
 import warnings
 from qtpy.QtCore import QObject, Property, Signal
+from qtpy.QtWidgets import QMessageBox
 from one.webclient import AlyxClient
 from requests import HTTPError
 
 
 class QAlyx(QObject):
+    """A Qt wrapper for :class:`one.webclient.AlyxClient`."""
+
+    tokenMissing = Signal(str)
     authenticationFailed = Signal(str)
     connectionFailed = Signal(str)
+    loggedIn = Signal(str)
+    loggedOut = Signal()
+    statusChanged = Signal(bool)
 
-    def __init__(self, *args, base_url: str, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, base_url: str, parent: QObject | None = None):
+        super().__init__(parent)
         self._client = AlyxClient(base_url=base_url, silent=True)
 
     @Property(AlyxClient)
@@ -23,8 +30,11 @@ class QAlyx(QObject):
         return self._client.is_logged_in
 
     def login(
-        self, username: str, password: str | None, cache_token: bool = False
-    ) -> bool:
+        self, username: str, password: str | None = None, cache_token: bool = False
+    ) -> None:
+        if self._client.is_logged_in and self._client.user == username:
+            return
+
         # try to authenticate. upgrade warnings to exceptions so we can catch them.
         try:
             with warnings.catch_warnings():
@@ -39,19 +49,32 @@ class QAlyx(QObject):
         # catch missing password / token
         except UserWarning as e:
             if 'No password or cached token' in e.args[0]:
-                self.authenticationFailed.emit(username)
+                self.tokenMissing.emit(username)
+                return
 
-        # catch connection issues
+        # catch connection issues: display a message box
         except ConnectionError as e:
-            # QErrorMessage().showMessage(e.args[0])
+            QMessageBox.critical(self.parent(), 'Connection Error', str(e))
             self.connectionFailed.emit(e.args[0])
+            return
 
         # catch authentication errors
         except HTTPError as e:
             if e.errno == 400:
+                print('auth')
                 self.authenticationFailed.emit(username)
+                return
             else:
                 raise e
 
-        # return outcome of login attempt
-        return self._client.is_logged_in and self._client.user == username
+        # emit signals
+        if self._client.is_logged_in and self._client.user == username:
+            self.statusChanged.emit(True)
+            self.loggedIn.emit(username)
+
+    def logout(self):
+        if not self._client.is_logged_in:
+            return
+        self._client.logout()
+        self.statusChanged.emit(False)
+        self.loggedOut.emit()
