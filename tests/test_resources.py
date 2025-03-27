a6c55efc125ed63b5f9fdee2ebd991bb08005693
Florian Rau <bimac@users.noreply.github.com>
2025-03-27 10:58:01 +0000

    tests for resources

---
+++ tests/test_resources.py
@@ -0,0 +1,57 @@
+import importlib
+import sys
+import xml.etree.ElementTree as ET
+from pathlib import Path
+from unittest.mock import patch
+
+from qtpy.QtGui import QIcon
+
+resources_path = Path(__file__).parents[1].joinpath('resources')
+
+
+class TestResources:
+    def test_rcc1(self):
+        """Test Resources (version 1)."""
+        with (
+            patch('iblqt.resources.QtCore.qVersion', return_value='5.7.0'),
+            patch('iblqt.resources.QtCore.qRegisterResourceData') as mock_register,
+            patch('iblqt.resources.QtCore.qUnregisterResourceData') as mock_unregister,
+        ):
+            if 'iblqt.resources' in sys.modules:
+                del sys.modules['iblqt.resources']
+            resources = importlib.import_module('iblqt.resources')
+            assert resources.rcc_version == 1
+            assert resources.qt_resource_struct is resources.qt_resource_struct_v1
+            mock_register.assert_called_once()
+            resources.qCleanupResources()
+            mock_unregister.assert_called_once()
+
+    def test_rcc2(self):
+        """Test Resources (version 2)."""
+        with (
+            patch('iblqt.resources.QtCore.qVersion', return_value='5.8.0'),
+            patch('iblqt.resources.QtCore.qRegisterResourceData') as mock_register,
+            patch('iblqt.resources.QtCore.qUnregisterResourceData') as mock_unregister,
+        ):
+            if 'iblqt.resources' in sys.modules:
+                del sys.modules['iblqt.resources']
+            resources = importlib.import_module('iblqt.resources')
+            assert resources.rcc_version == 2
+            assert resources.qt_resource_struct is resources.qt_resource_struct_v2
+            mock_register.assert_called_once()
+            resources.qCleanupResources()
+            mock_unregister.assert_called_once()
+
+    def test_resources(self, qtbot):
+        qrc_file = resources_path.joinpath('resources.qrc')
+        assert qrc_file.exists()
+        tree = ET.parse(qrc_file)
+        root = tree.getroot()
+        for resource in root.findall('qresource'):
+            prefix = resource.get('prefix')
+            for file in resource.findall('file'):
+                resource_path = resources_path.joinpath(file.text)
+                assert resource_path.exists()
+                resource_name = f':{prefix}/{resource_path.stem}'
+                icon = QIcon(resource_name)
+                assert len(icon.availableSizes()) > 0
