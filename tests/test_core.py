48e73cf2c6914ee0b1fe30d1dfbf76db57b6d7a8
Florian Rau <bimac@users.noreply.github.com>
2025-07-13 23:14:47 +0100

    Update test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -117,9 +117,9 @@ class TestDataFrameTableModel:
 
 
 class TestPathWatcher:
-    @pytest.mark.xfail(
-        reason='This fails with the GitHub Windows runner for some reason.'
-    )
+    # @pytest.mark.xfail(
+    #     reason='This fails with the GitHub Windows runner for some reason.'
+    # )
     def test_path_watcher(self, qtbot):
         parent = core.QObject()
         w = core.PathWatcher(parent=parent, paths=[])
